e729e806db70c73479b82711420e8b08583744fc
Justin Mills <justin.mills@waymarkcare.com>
2025-05-22 17:12:47 -0400

    pick up latest changes

--- run_web.py
+++
@@ -1,8 +0,0 @@
-import os
-from dotenv import load_dotenv
-from src.web.app import app
-
-if __name__ == "__main__":
-    load_dotenv()
-    port = int(os.getenv("WEB_PORT", 5000))
-    app.run(host="0.0.0.0", port=port, debug=True)
